f6442487f9ef62fc0e0b5299b421193e258d0d0a
madhur1807 <madhurmittal1807@gmail.com>
2018-02-14 19:33:21 +0530

    ..

---
+++ Experts/balance-transfer.php
@@ -0,0 +1,321 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- TITLE -->
+      <title>Balance Transfer</title>
+      <!-- FAVICON -->
+      <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />      
+      <!-- Bootstrap -->
+      <link href="css/bootstrap.min.css" rel="stylesheet">
+      <!--FONT AWESOME -->
+      <link href="css/font-awesome.css" rel="stylesheet">
+      <!-- MENU -->
+      <link rel="stylesheet" href="css/menuzord.css">       
+      <!-- STROKE GAP ICON -->
+      <link href="css/stroke-icon.css" rel="stylesheet">
+      <link href="css/demo.css" rel="stylesheet">
+      <link href="css/ie7.css" rel="stylesheet">
+      <!-- Portfolio Filter -->
+      <link rel="stylesheet" href="css/bootFolio.css">
+      <!-- Popup -->
+      <link rel="stylesheet" href="css/magnific-popup.css">      
+      <!-- JQUERY UI STYLE -->
+      <link rel="stylesheet" href="css/jquery-ui.css">        
+      <!-- OWL CSS -->
+      <link href="css/owl.theme.default.css" rel="stylesheet">
+      <link href="css/owl.carousel.css" rel="stylesheet">  
+      <!-- RS5.0 Main Stylesheet -->
+      <link rel="stylesheet" type="text/css" href="revolution/css/settings.css">    
+      <!-- RS5.0 Layers and Navigation Styles -->
+      <link rel="stylesheet" type="text/css" href="revolution/css/layers.css">
+      <link rel="stylesheet" type="text/css" href="revolution/css/navigation.css">           
+      <!-- MAIN STYLE -->
+      <link rel="stylesheet" href="css/style.css">
+
+      <!-- GOOGLE FONT -->
+      <link href='https://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
+      <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+      <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+        
+      <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+      <style type="text/css">
+        .features-ul p {
+          margin-bottom: 10px;
+          font-size: 14px;
+        }
+      </style>
+   </head>
+   <body>
+
+      <!-- =========================
+        START PRELOADER
+      ============================== -->
+      <div class="outslider_loading">
+         <div class="outslider_loader"></div>
+      </div>
+
+      <!-- =========================
+        END PRELOADER
+      ============================== -->
+
+      <!-- =========================
+        START HEADER SECTION
+      ============================== -->  
+      <?php include 'header.php';?>
+      <!-- =========================
+        END HEADER SECTION
+      ============================== -->
+
+      <!-- =========================
+        START PAGE TITLE SECTION
+      ============================== -->
+      <div class="page-title-area header-v-area">
+         <div class="container">
+            <div class="row">
+               <div class="col-sm-6 col-md-6">
+                  <div class="page-title-left">
+                     <h2>BALANCE TRANSFER</h2>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-6">
+                  <div class="page-bredcrumbs-area text-right">
+                     <ul  class="page-bredcrumbs">
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Project Financing</a></li>
+                        <li><a href="#">Balance Transfer</a></li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- =========================
+        END PAGE TITLE SECTION
+      ============================== -->      
+
+      <!-- =========================
+        START FEATURE TYPOGRAPHY SECTION
+      ============================== -->         
+      <section class="feature-typo">
+           <div class="container">
+               <div class="row">
+                   <div class="col-md-12">
+                       <div class="f-h1-head">
+                           <h1>What exactly is a balance transfer?  </h1>
+                           <p>A balance transfer is the transfer of (part of) the balance (either money or credit) in an account to another account, often held at another institution. It is most commonly used when describing a credit card balance transfer.<br>Balance transfer allows people to move their debts such as credit card balances, student loans, home loan medical bills, car loans to a zero or lower interest rate credit card for a promotional or limited period. The overall amount and the types of balances you can transfer depends on your credit card as well as your credit score. Moreover, balance transfer should be done as per the timings allocated by the credit card company.<br>While many credit card issuers offer 0% interest balance transfers, it is important to note that some issuers also charge a transfer fee, which could range from 0–5%. As a result, consumers should evaluate the balance transfer interest rate during the promotional period, the length of the promotional period, and the balance transfer fee when deciding on which balance transfer promotion is best.</p>
+                       </div>
+                   </div>
+                   <div class="col-md-12">
+                       <div class="f-h1-head f-h2-head">
+                          <h3>How do I complete a balance transfer ?</h3>
+                          <ul class="features-ul">
+                            <li><p>When you respond to a balance transfer offer, you’ll indicate who you want to pay, the account numbers, and how much you want to transfer.</p></li>
+                            <li><p> Once you’re approved for the balance transfer, the credit card company contacts your creditors or billers on your behalf and pays them the amount you indicated. It can take up to two weeks for this process.</p></li>  
+                            <li><p>If you have any payments due before that time, you’ll want to go ahead and make those payments by their due dates to avoid late fees. </p></li>
+                            <li><p>Credit card balance transfers may charge a balance transfer fee—usually 3% to 5% of transferred debt. </p></li>
+                          </ul>
+                       </div>
+                   </div> 
+                   <div class="col-md-12">
+                       <div class="f-h1-head f-h2-head">
+                           <h3>What are the benefits of a balance transfer?</h3>
+                           <ul class="features-ul">
+                            <li><p> A low balance transfer APR can help you catch up on your existing debt. That’s because you can get a low promo or introductory APR to pay down that balance for a defined time frame, like 12 months.</p></li>
+                            <li><p>A low promo or introductory APR may also help cut the time it takes to reduce your debt. When you pay high APRs, a lot of your payment goes to the interest rather than paying down the principal balance itself.</p></li>  
+                            <li><p>Finally, instead of paying multiple creditors on multiple due dates, consolidating all of your balances onto one card with a low or 0% promo or introductory balance transfer APR means you only have to keep track of one payment a month—and not multiple cards with multiple due dates.</p></li>
+                          </ul>
+                   </div>                                                                            
+               </div>
+               <div class="row">
+                   <div class="col-sm-6 col-md-6">
+                       <div class="typo-column" style="padding: 5px">
+                           <h3 style="font-weight: bold;text-decoration: underline;">What types of existing balances can I transfer?</h3>
+                           <p>You can transfer the existing balances off of your store credit cards, gas cards and other cards. Just remember, when you are approved for a credit card, you will be given a certain limit and you can only transfer up to that amount – be sure to account for any balance transfer fees. The typical balance transfer fee in 2017 was 3%.</p>
+                       </div>
+                   </div>
+                   <div class="col-sm-6 col-md-6">
+                       <div class="typo-column" style="padding: 5px">
+                           <h3 style="font-weight: bold;text-decoration: underline;">What are the different kinds of balance transfer rates available?</h3>
+                           <p>Credit card companies offer incentives—like a low interest rate on purchases and balance transfers—to encourage you to transfer your business to them, and hopefully establish a long-lasting relationship.The best balance transfer rates could be offered by credit card companies to new cardmembers.</p>
+                       </div>
+                   </div>                
+               </div> 
+             </div>
+      </section>
+      <!-- =========================
+        END FEATURE TYPOGRAPHY SECTION
+      ============================== --> 
+
+      <!-- =========================
+        START FOOTER SECTION
+      ============================== -->
+      <footer class="footer-area">
+         <div class="footer-top-content">
+            <div class="container">
+               <div class="row">
+                  <div class="col-sm-4 col-md-4 no-padding">
+                     <div class="footer-top-single text-center">
+                        <p>Call Us Now</p>
+                        <p>Office Telephone : 002 01065370701</p>
+                        <p>Mobile : 002 01065370701</p>
+                     </div>
+                  </div>
+                  <div class="col-sm-4 col-md-4 no-padding">
+                     <div class="footer-top-single footer-top-single-bg-2 text-center">
+                        <p>Call Us Now</p>
+                        <p>Office Telephone : 002 01065370701</p>
+                        <p>Mobile : 002 01065370701</p>
+                     </div>
+                  </div>
+                  <div class="col-sm-4 col-md-4 no-padding">
+                     <div class="footer-top-single text-center">
+                        <p>Call Us Now</p>
+                        <p>Office Telephone : 002 01065370701</p>
+                        <p>Mobile : 002 01065370701</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="footer-main-content">
+            <div class="container">
+               <div class="row">
+                  <div class="col-sm-6 col-md-3">
+                     <div class="footer-main-single footer-first-content clearfix">
+                        <h2>About Experts</h2>
+                        <p>Free and impartial money advice, set up by government, advice & guides to help improve your finances.</p>
+                        <div class="footer-time">
+                           <ul>
+                              <li>Monday to Friday</li>
+                              <li>8:00 - 16:00</li>
+                              <li>Saturday</li>
+                              <li>8:00 - 16:00</li>
+                              <li>Sunday and Bank</li>
+                              <li>closed</li>
+                           </ul>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-sm-6 col-md-3">
+                     <div class="footer-main-single clearfix">
+                        <h2>Services</h2>
+                        <div class="footer-service">
+                           <ul>
+                              <li><a href="#">Insurance</a></li>
+                              <li><a href="#">Business Loans</a></li>
+                              <li><a href="#">Investments</a></li>
+                              <li><a href="#">Personal Finance</a></li>
+                              <li><a href="#">Tax Help</a></li>
+                              <li><a href="#">Credit Cards</a></li>
+                              <li><a href="#">Overdraft</a></li>
+                              <li><a href="#">Deposits</a></li>
+                              <li><a href="#">Mortgage</a></li>
+                           </ul>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-sm-6 col-md-3">
+                     <div class="footer-main-single footer-third-content clearfix">
+                        <h2>Recent Posts</h2>
+                        <div class="recent-post-details">
+                           <i class="fa fa-image"></i>
+                           <h2><a href="#">Four ways to cheer yourself up on Blue Monday!</a></h2>
+                           <p>Feb 22, 2016</p>
+                        </div>
+                        <div class="recent-post-details recent-post-details-no-border">
+                           <i class="fa fa-briefcase"></i>
+                           <h2><a href="#">In the news: this week’s top money stories</a></h2>
+                           <p>Feb 20, 2016</p>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-sm-6 col-md-3">
+                     <div class="footer-main-single footer-first-content f-fourth-content clearfix">
+                        <h2>Newsletter</h2>
+                        <p>Don’t miss to subscribe to our new feeds, kindly fill the form below.</p>
+                        <form>
+                           <div class="form-group footer-subscribe">
+                              <input type="email" class="form-control" placeholder="Subscribe Our Newsletter">
+                              <button type="submit" class="btn btn-default"><i class="fa fa-long-arrow-right"></i></button>
+                           </div>
+                        </form>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="footer-bottom-content-details">
+            <div class="container">
+               <div class="row">
+                  <div class="footer-bottom-content">
+                     <div class="col-sm-4 col-md-4 no-padding-left">
+                        <div class="footer-bottom-single">
+                           <p> © 2016 - 2017 Experts. With Love by <a href="http://themeforest.net/user/7oroof/portfolio?ref=7oroof">7oroof.com</a></p>
+                        </div>
+                     </div>
+                     <div class="col-sm-4 col-md-4 no-padding">
+                        <div class="footer-bottom-single">
+                           <div class="footer-menu">
+                              <a href="#">About</a>
+                              <a href="#">Blog</a>
+                              <a href="#">Privacy Policy</a>
+                              <a href="#">Terms</a>
+                              <a href="#"> Contact</a>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="col-sm-4 col-md-4  no-padding-right">
+                        <div class="footer-bottom-single">
+                           <ul class="footer-social text-right">
+                              <li> <a href="#"><i class="fa fa-facebook"></i></a></li>
+                              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                              <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                              <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                              <li><a href="#"> <i class="fa fa-rss"></i></a></li>
+                           </ul>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- =========================
+        END FOOTER SECTION
+      ============================== -->
+
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+      <script src="js/jquery-1.11.3.min.js"></script>        
+      <!-- RS5.0 Core JS Files -->
+      <script type="text/javascript" src="revolution/js/jquery.themepunch.tools.min.js?rev=5.0"></script>
+      <script type="text/javascript" src="revolution/js/jquery.themepunch.revolution.min.js?rev=5.0"></script>
+      <script type="text/javascript" src="revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+      <script type="text/javascript" src="revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+      <script type="text/javascript" src="revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+      <script type="text/javascript" src="revolution/js/extensions/revolution.extension.parallax.min.js"></script>          
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+      <script src="js/bootstrap.min.js"></script>
+      <script src="js/menuzord.js"></script>
+      <script src="js/owl.carousel.min.js"></script>
+      <script src="js/jquery.counterup.js"></script> 
+      <script src="js/waypoints.min.js"></script> 
+      <script src="js/countdown.js"></script> 
+      <script src="js/parallax.min.js"></script>      
+      <script src="js/jquery.bootFolio.js"></script>
+      <script src="js/jquery.magnific-popup.js"></script>
+      <script src="https://maps.googleapis.com/maps/api/js"></script>       
+      <script src="js/jquery-ui.js"></script>
+      <script src="js/rev-function.js"></script>     
+      <script src="js/smoothscroll.js"></script>      
+      <script src="js/jquery.matchHeight.js"></script>       
+      <script src="js/main.js"></script>       
+   </body>
+</html>
